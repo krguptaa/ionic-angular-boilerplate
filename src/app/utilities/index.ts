c776c7b2ad2f3eb04463a5954f6903ab41cd1bbf
Kamlesh Gupta <ronak@bitbucket.com>
2025-09-08 13:44:59 +0530

    Solved the compile issue

---
+++ src/app/utilities/index.ts
@@ -0,0 +1,12 @@
+// Re-export all utility classes for backward compatibility
+export { DateUtils } from './date-utils';
+export { ValidationUtils } from './validation-utils';
+export { PlatformUtils } from './platform-utils';
+export { StorageUtils } from './storage-utils';
+export { StringUtils } from './string-utils';
+export { ErrorUtils } from './error-utils';
+export { FileUtils } from './file-utils';
+export { ColorUtils } from './color-utils';
+export { NumberUtils } from './number-utils';
+export { HttpUtils } from './http-utils';
+export { AsyncUtils } from './async-utils';
